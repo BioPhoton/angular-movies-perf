8bd10e16977a29871a9e6afe93dc210357ec19b1
Michael Hladky <michael@hladky.at>
2021-07-05 00:36:53 +0200

    chore: test lhci filesystem

--- lighthouserc.github.js
+++ lighthouserc.github.js
@@ -4,7 +4,8 @@ module.exports = {
       staticDistDir: "dist/hub-movies",
     },
     upload: {
-      target: "temporary-public-storage",
+      target: "filesystem",
+      outputDir: ".lighthouseci"
     },
   },
 };
