eb2b76a598eb6e8bfc8ed6a359e12f890c342a64
Michael Hladky <michael@hladky.at>
2021-07-05 02:31:38 +0200

    chore: test lhci filesystem 7

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,9 +5,7 @@ module.exports = {
       staticDistDir: "dist/hub-movies",
     },
     upload: {
-      target: "lhci",
-      serverBaseUrl: "http://localhost:9001",
-      // token: process.env.LHCI_TOKEN, // "98298654-3bb3-46e3-96f2-58058d5d5edb" could also use LHCI_TOKEN variable instead
+      target: "temporary-public-storage",
     },
   },
 };
