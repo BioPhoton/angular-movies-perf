42d429b5c0709456879481e78aa80fb2849fde94
Michael Hladky <michael@hladky.at>
2021-07-05 02:40:48 +0200

    chore: adopt lhci-action 1

--- lighthouserc.github.js
+++ lighthouserc.github.js
@@ -2,11 +2,6 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: "dist/hub-movies",
-    },
-    upload: {
-      target: "temporary-public-storage",
-      // target: "filesystem",
-      // outputDir: ".lighthouseci"
-    },
+    }
   },
 };
