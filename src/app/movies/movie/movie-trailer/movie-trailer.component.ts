a24078ea7668e47b62cc1e8870e1a9a56e67c293
Michael Hladky <michael@hladky.at>
2021-07-06 04:28:17 +0200

    fix> lint issues

--- src/app/movies/movie/movie-trailer/movie-trailer.component.ts
+++ src/app/movies/movie/movie-trailer/movie-trailer.component.ts
@@ -1,4 +1,5 @@
 import {ChangeDetectionStrategy, Component, Inject} from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
@@ -26,6 +27,8 @@ export class MovieTrailerComponent {
   url: SafeResourceUrl;
 
   constructor(
+    // tslint:disable-next-line: no-unused-variable
+    public dialogRef: MatDialogRef<MovieTrailerComponent>,
     @Inject(MAT_DIALOG_DATA) private data: any,
     private dom: DomSanitizer ) {
     this.url = this.dom.bypassSecurityTrustResourceUrl(this.data.url);
