9cc4221d9a13b192887b5e2ac9f2476c5b160e59
Michael Hladky <michael@hladky.at>
2021-07-07 03:34:02 +0200

    docs: add comment to lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,11 +3,15 @@ module.exports = {
   ci: {
     collect: {
       numberOfRuns: 10,
+      // needed as auto discovery is not able to find it
       staticDistDir: "dist/hub-movies",
     },
     upload: {
+      // save target is a custom lhci server
       target: "lhci",
+      // url of the local server
       serverBaseUrl: "http://localhost:9001",
+      // token retrieved in the setup and connect step
       token: "98298654-3bb3-46e3-96f2-58058d5d5edb" // could also use LHCI_TOKEN variable instead
     },
   },
