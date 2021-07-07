83a78c7b31125e0cff682b0ea828a4dd1eef4438
Julian Jandl <julian@jandl.wien>
2021-07-07 09:28:15 +0200

    chore: fix lint

--- src/app/firebase-app.ts
+++ src/app/firebase-app.ts
@@ -1,6 +1,10 @@
-import { Injectable, InjectionToken } from '@angular/core';
-import { initializeApp, FirebaseApp } from 'firebase/app';
+import { InjectionToken } from '@angular/core';
+import { FirebaseApp } from 'firebase/app';
 import { FirebaseFirestore } from 'firebase/firestore';
 
-export const MoviesFirebase = new InjectionToken<FirebaseApp>('movies-firebase-app');
-export const MoviesFirestore = new InjectionToken<FirebaseFirestore>('movies-firestore');
+export const MoviesFirebase = new InjectionToken<FirebaseApp>(
+  'movies-firebase-app'
+);
+export const MoviesFirestore = new InjectionToken<FirebaseFirestore>(
+  'movies-firestore'
+);
