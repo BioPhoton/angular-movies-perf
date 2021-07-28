efebecae9e52e66b84441cdc354b1e060c72a3c6
Michael Hladky <michael@hladky.at>
2021-07-29 01:36:13 +0200

    perf: test netlify overlay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
